2cf7922e675b9fb6934c39b8df9360691cbb95cf
mrelectronst <fhkaraman@outlook.com>
2023-07-31 12:04:16 +0300

    Add project files.

---
+++ App/ArrayProcess.cs
@@ -0,0 +1,89 @@
+﻿namespace App;
+
+public static class ArrayProcess
+{
+    static char[] validChars = { '0', '1', ',', ';' };
+
+    public static bool IsInputStringValid(string? inputString)
+    {
+        if (string.IsNullOrEmpty(inputString))
+            return false;
+        bool containsValidChars = inputString.ToCharArray().All(c => validChars.Contains(c));
+        return containsValidChars;
+    }
+
+    public static int GetAreasCount(string matrixString)
+    {
+        var matrix = ConvertMatrix(matrixString);
+        return CountAreas(matrix);
+    }
+
+    static int[][] ConvertMatrix(string matrixString)
+    {
+        string[] rows = matrixString.Split(';');
+        int rowCount = rows.Length;
+        int[][] matrix = new int[rowCount][];
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            string[] rowValues = rows[i].Split(',');
+            int columnCount = rowValues.Length;
+            matrix[i] = new int[columnCount];
+
+            for (int j = 0; j < columnCount; j++)
+            {
+                int value = int.Parse(rowValues[j]);
+                matrix[i][j] = value;
+            }
+        }
+        return matrix;
+    }
+
+    static int CountAreas(int[][] matrix)
+    {
+        int areaCount = 0;
+        int rowCount = matrix.Length;
+        int columnCount = matrix[0].Length;
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                if (matrix[i][j] == 1)
+                {
+                    areaCount++;
+                    ScoreArea(matrix, i, j);
+                }
+            }
+        }
+        return areaCount;
+    }
+
+    static void ScoreArea(int[][] matrix,
+                          int row,
+                          int column)
+    {
+        if (IsOutOfBorders(matrix, row, column))
+            return;
+
+        matrix[row][column] = 0;
+        ScoreArea(matrix, row - 1, column);
+        ScoreArea(matrix, row + 1, column);
+        ScoreArea(matrix, row, column - 1);
+        ScoreArea(matrix, row, column + 1);
+    }
+
+    static bool IsOutOfBorders(int[][] matrix,
+                               int row,
+                               int column)
+    {
+        int rowCount = matrix.Length;
+        int columnCount = matrix[0].Length;
+        return (row < 0 ||
+                row >= rowCount ||
+                column < 0 ||
+                column >= columnCount ||
+                matrix[row][column] == 0);
+    }
+}
+
---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿using App;
+
+while (true)
+{
+    Console.WriteLine("Write your matrix (e.g., '1,0,1;1,1,0'): ");
+    string? inputString = Console.ReadLine();
+    if (!ArrayProcess.IsInputStringValid(inputString))
+    {
+        Console.WriteLine("Invalid Value");
+        return;
+    }
+    var areaCount = ArrayProcess.GetAreasCount(inputString);
+    Console.WriteLine($"Number of areas formed of number 1: {areaCount}\n");
+}
